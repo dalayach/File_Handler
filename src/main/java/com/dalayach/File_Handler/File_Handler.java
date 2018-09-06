12d0176ecac06a69ed3cd43874da955b7268ffdc
dalayach <dalayach@masonlive.gmu.edu>
2018-09-06 12:57:53 -0400

    Still in the initial commit phase

---
+++ src/main/java/com/dalayach/File_Handler/File_Handler.java
@@ -0,0 +1,20 @@
+package com.dalayach.File_Handler;
+
+public class File_Handler
+{
+
+   public File_Handler()
+   {
+   
+   
+   
+   }
+
+   public File_Handler(String filename)
+   {
+   
+   
+   
+   }
+
+}
